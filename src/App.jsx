b55a8f9f29602753829ec3188c06f3d939dad39d
siwill9163 <simon.williams1985@outlook.com>
2024-12-04 18:17:57 +0000

    first go

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import CryptoJS from "crypto-js";
 
-function App() {
-  const [count, setCount] = useState(0)
+const SECRET_PASS = "XkhZG4fW2t2W"
+
+const App = () => {
+
+const [screen, setScreen] = useState("encrypt");
+const [text, setText] = useState("");
+const [errorMessage, setErrorMessage] = useState("");
+
+const [encryptedData, setEncyptedData] = useState("");
+
+const [decryptedData, setDecryptedData] = useState("");
+
+
+
+const switchScreen = (type) => {
+  setScreen(type);
+};
+
+const encryptData = () => {
+  try {
+    const data = CryptoJS.AES.encrypt(
+      JSON.stringify(text),
+      SECRET_PASS
+    ).toString();
+    setEncyptedData(data);
+    setErrorMessage("");    
+  } catch (error){
+    setErrorMessage("failed")
+  }
+};
+
+const decryptData = () => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(text, SECRET_PASS);
+    const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    setDecryptedData(data);
+    setErrorMessage("");
+  }catch(error){
+    setErrorMessage("Decryption failed")
+  }
+}
+
+
+const handleClick = () => {
+
+  if (!text){
+    setErrorMessage("Please enter some text");
+    return;
+  }
+
+  if (screen === "encrypt"){
+    encryptData();
+  } else {
+    decryptData();
+  }
+}
 
   return (
-    <>
+    <div className='container'>
+     
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className={`btn btn-left ${screen === "encrypt" ? "active" : ""}`}
+        onClick={()=>{switchScreen("encrypt")
+
+        }}>
+          Encrypt message
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <button className={`btn btn-right ${screen === "decrypt" ? "active" : ""}`}
+        onClick={()=>{switchScreen("decrypt")
+
+        }}>
+          Decrypt message
+        </button>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="card">
+        <textarea
+          value={text}
+          onChange={({target}) => setText(target.value)}
+          placeholder={screen === "encrypt" ? "Enter Your Text" : "Enter Encrpted Data"
+
+          }
+        />
+      
+        {errorMessage && <div className='error'> {errorMessage}</div>}
+
+      <button className={`btn submit-btn ${
+        screen === "encrypt" ? "encrypt-btn" : "decrypt-btn"
+      }`}
+      onClick={handleClick}
+      >
+        {screen === "encrypt" ? "ENCRYPT YOUR MESSAGE" : "DECRYPT A MESSAGE"}
+      
+      </button>
+    </div>
+    {encryptData || decryptData ? (
+      <div className="content">
+        <label>{screen === "encrypt" ? "ENCRYPTED" : "Decrypted"} DATA WILL SHOW BELOW...</label>
+        <p>{screen === "encrypt" ? encryptedData : decryptedData}</p>
+      </div>
+    ) : null}
+    </div>
   )
 }
 
